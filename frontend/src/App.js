451b1dfca780053ba412a7bea058925cd1dca16d
Detectheif <hbj970417@naver.com>
2025-05-27 20:17:34 +0900

    App.js 에서 토큰 추출

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,21 @@ import LiveEmotionPlayer from "./components/LiveEmotionPlayer";
 import FitbitCallback from "./components/FitbitCallback";
 
 function App() {
+  useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.search);
+    let token = queryParams.get("access_token");
+
+    if (!token && window.location.hash) {
+      const hashParams = new URLSearchParams(window.location.hash.substring(1));
+      token = hashParams.get("access_token");
+    }
+
+    if (token) {
+      localStorage.setItem("access_token", token);
+      window.history.replaceState({}, document.title, "/");
+    }
+  }, []);
+  
   const [emotion, setEmotion] = useState("보통");
 
   return (
